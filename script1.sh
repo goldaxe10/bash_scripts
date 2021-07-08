882bb50ee2342273e58a27d78d8b3bd1d2ae8163
V <you@example.com>
2021-07-09 01:21:56 +0500

    Ушёл спать

--- script1.sh
+++ script1.sh
@@ -1,43 +1,46 @@
 #!/bin/bash
-set -euo pipefail
-
-arr_url=()
-echo "Count of requests: "
-read cnt_req
-while true; do
-	read url
-	if [ "$url" != "" ]
-	then
-		arr_url+=( "$url" )
-	else 
-		break
-	fi
-done
-echo ${arr_url[@]}
-
-
-
-
-
-
-# arr=(Hello World !) 
-# for item in ${arr[@]}; do
-	# echo $item
-# done
-
-
 # set -euo pipefail
-# cnt_of_requests=100 #$1
 
+# arr_url=()
+# echo "Count of requests:"
+read cnt_req
+arr_metrics=("%{time_namelookup}" 
+			"%{time_connect}" 
+			"%{time_appconnect}" 
+			"%{time_pretransfer}" 
+			"%{time_starttransfer}")
 
-# url_1="https://00.img.avito.st/image/1/1cs1qbaxeSIDHvsvE_qxrtcKeSaJCHMg" #$2
-# url_2="https://68.img.avito.st/image/1/rS2IB7axAcS-sIPJ3gH0BXGkAcA0pgvG" #$3
-# i=0
-
-
-# while [ "$i" -le 7000 ]; do
-	
-	# i=$(( i+1 ))
+# # Заполняем массив URL'ами, на которые будем делать запросы
+# echo "Insert the URLs:"
+# while true; do
+# 	read url
+# 	if [ "$url" != "" ]
+# 	then
+# 		arr_url+=( "$url" )
+# 	else 
+# 		break
+# 	fi
 # done
+# echo ${arr_url[@]}
 
+arr_url=("https://68.img.avito.st/image/1/rS2IB7axAcS-sIPJ3gH0BXGkAcA0pgvG" "https://00.img.avito.st/image/1/1cs1qbaxeSIDHvsvE_qxrtcKeSaJCHMg")
+#url1="https://68.img.avito.st/image/1/rS2IB7axAcS-sIPJ3gH0BXGkAcA0pgvG"
+#url2="https://00.img.avito.st/image/1/1cs1qbaxeSIDHvsvE_qxrtcKeSaJCHMg"
 
+# y=$(curl -o /dev/null -w "%{time_namelookup}" $url1)
+# echo "=============="
+# echo "Metric: time_namelookup"
+# echo $y
+
+metrics(){
+curl -o /dev/null -w $1 $2
+}
+
+# time_namelookup=$(metrics "%{time_namelookup}" $url1) 
+# echo $time_namelookup
+
+for cnt in cnt_req; do
+	for url in ${arr_url[@]}; do
+	echo $url
+	done
+done
