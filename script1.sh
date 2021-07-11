3ea1a11c9b099453f4fd5a5ca207e664f25ad11d
goldaxe10 <zorg05mail@gmail.com>
2021-07-11 11:01:49 +0500

    Добавил в скрипт запись полученных данных (метрик) в массив

--- script1.sh
+++ script1.sh
@@ -31,35 +31,26 @@ arr_metrics=("%{time_namelookup}"
 # echo ${arr_url[@]}
 
 arr_url=("https://68.img.avito.st/image/1/rS2IB7axAcS-sIPJ3gH0BXGkAcA0pgvG" "https://00.img.avito.st/image/1/1cs1qbaxeSIDHvsvE_qxrtcKeSaJCHMg")
-url1="https://68.img.avito.st/image/1/rS2IB7axAcS-sIPJ3gH0BXGkAcA0pgvG"
-#url2="https://00.img.avito.st/image/1/1cs1qbaxeSIDHvsvE_qxrtcKeSaJCHMg"
 
 # y=$(curl -o /dev/null -w "%{time_namelookup}" $url1)
 # echo "=============="
 # echo "Metric: time_namelookup"
 # echo $y
 
-# metrics_func(){
-# curl -o /dev/null -w $1 $2
-# sleep 2
-# }
-
 metrics_func(){
-curl -o /dev/null -w "${arr_metrics[0]} ${arr_metrics[1]} ${arr_metrics[2]} ${arr_metrics[3]} ${arr_metrics[4]}" $url1
-sleep 2
+curl -o /dev/null -w "${arr_metrics[0]} ${arr_metrics[1]} ${arr_metrics[2]} ${arr_metrics[3]} ${arr_metrics[4]}" $1
+sleep 1
 }
 
-q=$(metrics_func)
-echo $q
-for metric in $q; do
-	echo $metric
+for ((cnt=1; cnt<=$cnt_req; cnt++)); do
+	for url in ${arr_url[@]}; do
+		arr_results=("res")
+		q=$(metrics_func $url)
+		for metric in $q; do
+			arr_results+=( "$metric" )
+			# if [ $metric == ]
+		done
+		echo ${arr_results[@]}
+	done		
 done
 
-# time_namelookup=$(metrics "%{time_namelookup}" $url1) 
-# echo $time_namelookup
-
-# for ((cnt=1; cnt<=$cnt_req; cnt++)); do
-# 	for url in ${arr_url[@]}; do
-# 		echo
-# 	done		
-# done
